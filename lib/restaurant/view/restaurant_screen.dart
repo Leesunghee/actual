669543580a7d7ad817ec8bdefff2a4c6aaf7b53d
leesunghee <realwunz@gmail.com>
2023-08-06 22:46:50 +0900

    레스토랑 상세페이 작업하기

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,6 +1,7 @@
 import 'package:actual/common/const/data.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/model/restaurant_model.dart';
+import 'package:actual/restaurant/view/restaurant_detail_screen.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -26,34 +27,45 @@ class RestaurantScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: FutureBuilder<List> (
-            future: paginateRestaurant(),
-            builder: (context, AsyncSnapshot<List> snapshot) {
-              // print(snapshot.error);
-              // print(snapshot.data);
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: FutureBuilder<List>(
+          future: paginateRestaurant(),
+          builder: (context, AsyncSnapshot<List> snapshot) {
+            // print(snapshot.error);
+            // print(snapshot.data);
 
-              if (!snapshot.hasData) {
-                return Container();
-              } 
-              
-              return ListView.separated(
-                  itemBuilder: (_, index) {
-                    final item = snapshot.data![index];
-                    final pItem = RestaurantModel.fromJson(json: item,);
+            if (!snapshot.hasData) {
+              return Container();
+            }
 
-                    return RestaurantCard.fromModel(model: pItem);
-                  },
-                  separatorBuilder: (_, index) {
-                    return const SizedBox(height: 16.0,);
-                  },
-                  itemCount: snapshot.data!.length,
-              );
-            },
-          ),
-        )
-      ),
+            return ListView.separated(
+              itemBuilder: (_, index) {
+                final item = snapshot.data![index];
+                final pItem = RestaurantModel.fromJson(
+                  json: item,
+                );
+
+                return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (_) => RestaurantDetailScreen(),
+                        ),
+                      );
+                    },
+                    child: RestaurantCard.fromModel(model: pItem));
+              },
+              separatorBuilder: (_, index) {
+                return const SizedBox(
+                  height: 16.0,
+                );
+              },
+              itemCount: snapshot.data!.length,
+            );
+          },
+        ),
+      )),
     );
   }
 }

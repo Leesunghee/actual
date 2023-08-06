1631b777ffd92adfd1894724dd5042603c4cc4fb
leesunghee <realwunz@hyundai-autoever.com>
2023-08-07 07:55:20 +0900

    ProductCard 작업하기

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:actual/product/component/product_card.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:flutter/material.dart';
 
@@ -13,18 +14,20 @@ class RestaurantDetailScreen extends StatelessWidget {
       child: Column(
         children: [
           RestaurantCard(
-              image: Image.asset(
-                'asset/img/food/ddeok_bok_gi.jpg'
-              ),
-              name: '불타는 떡볶이',
-              tags: ['떡볶이', '맛있음', '치즈'],
-              ratingsCount: 100,
-              deliveryTime: 30,
-              deliveryFee: 3000,
-              ratings: 4.76,
-              isDetail: true,
-              detail: '맛있는 떡볶이',
-          )
+            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+            name: '불타는 떡볶이',
+            tags: ['떡볶이', '맛있음', '치즈'],
+            ratingsCount: 100,
+            deliveryTime: 30,
+            deliveryFee: 3000,
+            ratings: 4.76,
+            isDetail: true,
+            detail: '맛있는 떡볶이',
+          ),
+          const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: ProductCard()
+          ),
         ],
       ),
     );

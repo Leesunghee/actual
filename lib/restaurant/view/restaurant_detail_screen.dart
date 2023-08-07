438746c35bfb26611afbed7a5e8d16c4cfb97b94
leesunghee <realwunz@gmail.com>
2023-08-07 21:56:06 +0900

    ProductCard 매핑하기

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -44,7 +44,7 @@ class RestaurantDetailScreen extends StatelessWidget {
             slivers: [
               renderTop(model: item),
               renderLabel(),
-              renderProducts(),
+              renderProducts(products: item.products),
             ],
           );
         },
@@ -67,16 +67,21 @@ class RestaurantDetailScreen extends StatelessWidget {
     );
   }
 
-  SliverPadding renderProducts() {
+  SliverPadding renderProducts({
+    required List<RestaurantProductModel> products,
+  }) {
     return SliverPadding(
       padding: EdgeInsets.symmetric(horizontal: 16.0),
       sliver: SliverList(
         delegate: SliverChildBuilderDelegate(
           (context, index) {
-            return const Padding(
-                padding: EdgeInsets.only(top: 16.0), child: ProductCard());
+            final model = products[index];
+
+            return Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: ProductCard.fromModel(model: model));
           },
-          childCount: 10,
+          childCount: products.length,
         ),
       ),
     );
